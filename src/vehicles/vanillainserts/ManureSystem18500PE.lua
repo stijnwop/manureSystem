0531adc2dd222cd432049cf1bf07030812c8e3e6
Stijn Wopereis <stijnwop@gmail.com>
2019-10-17 19:40:46 +0200

    Add insertions for the zunhammer 18500PE and Veenhuis Premium.

---
+++ src/vehicles/vanillainserts/ManureSystem18500PE.lua
@@ -0,0 +1,35 @@
+----------------------------------------------------------------------------------------------------
+-- ManureSystem18500PE
+----------------------------------------------------------------------------------------------------
+-- Purpose: Insert Manure System function into the Zunhammer 18500PE.
+--
+-- Copyright (c) Wopster, 2019
+----------------------------------------------------------------------------------------------------
+
+---@class ManureSystem18500PE
+ManureSystem18500PE = {}
+
+function ManureSystem18500PE.prerequisitesPresent(specializations)
+    return true
+end
+
+function ManureSystem18500PE.registerEventListeners(vehicleType)
+    SpecializationUtil.registerEventListener(vehicleType, "onPreLoad", ManureSystem18500PE)
+end
+
+function ManureSystem18500PE:onPreLoad(savegame)
+    setXMLString(self.xmlFile, ("vehicle.manureSystemConnectors.connector(%d)#type"):format(0), "coupling")
+    setXMLBool(self.xmlFile, ("vehicle.manureSystemConnectors.connector(%d)#createNode"):format(0), true)
+    setXMLString(self.xmlFile, ("vehicle.manureSystemConnectors.connector(%d)#position"):format(0), "1.261 0.821 2.797")
+    setXMLString(self.xmlFile, ("vehicle.manureSystemConnectors.connector(%d)#rotation"):format(0), "25 90 0")
+
+    --setXMLString(self.xmlFile, ("vehicle.manureSystemConnectors.connector(%d)#type"):format(1), "coupling")
+    --setXMLBool(self.xmlFile, ("vehicle.manureSystemConnectors.connector(%d)#createNode"):format(1), true)
+    --setXMLString(self.xmlFile, ("vehicle.manureSystemConnectors.connector(%d)#position"):format(1), "0.499 1.174 -2.965")
+    --setXMLString(self.xmlFile, ("vehicle.manureSystemConnectors.connector(%d)#rotation"):format(1), "14.5 180 0")
+    --
+    --setXMLString(self.xmlFile, ("vehicle.manureSystemConnectors.connector(%d)#type"):format(2), "coupling")
+    --setXMLBool(self.xmlFile, ("vehicle.manureSystemConnectors.connector(%d)#isParkPlace"):format(2), true)
+    --setXMLBool(self.xmlFile, ("vehicle.manureSystemConnectors.connector(%d)#createNode"):format(2), true)
+    --setXMLString(self.xmlFile, ("vehicle.manureSystemConnectors.connector(%d)#position"):format(2), "-0.95 1.73 -2.65")
+end
---
+++ src/vehicles/vanillainserts/ManureSystemPremiumIntegral30000.lua
@@ -0,0 +1,35 @@
+----------------------------------------------------------------------------------------------------
+-- ManureSystemPremiumIntegral30000
+----------------------------------------------------------------------------------------------------
+-- Purpose: Insert Manure System function into the Veenhuis PremiumIntegral30000.
+--
+-- Copyright (c) Wopster, 2019
+----------------------------------------------------------------------------------------------------
+
+---@class ManureSystemPremiumIntegral30000
+ManureSystemPremiumIntegral30000 = {}
+
+function ManureSystemPremiumIntegral30000.prerequisitesPresent(specializations)
+    return true
+end
+
+function ManureSystemPremiumIntegral30000.registerEventListeners(vehicleType)
+    SpecializationUtil.registerEventListener(vehicleType, "onPreLoad", ManureSystemPremiumIntegral30000)
+end
+
+function ManureSystemPremiumIntegral30000:onPreLoad(savegame)
+    setXMLString(self.xmlFile, ("vehicle.manureSystemConnectors.connector(%d)#type"):format(0), "coupling")
+    setXMLBool(self.xmlFile, ("vehicle.manureSystemConnectors.connector(%d)#createNode"):format(0), true)
+    setXMLString(self.xmlFile, ("vehicle.manureSystemConnectors.connector(%d)#position"):format(0), "1.2 0.39 3.253")
+    setXMLString(self.xmlFile, ("vehicle.manureSystemConnectors.connector(%d)#rotation"):format(0), "11 85 0")
+
+    --setXMLString(self.xmlFile, ("vehicle.manureSystemConnectors.connector(%d)#type"):format(1), "coupling")
+    --setXMLBool(self.xmlFile, ("vehicle.manureSystemConnectors.connector(%d)#createNode"):format(1), true)
+    --setXMLString(self.xmlFile, ("vehicle.manureSystemConnectors.connector(%d)#position"):format(1), "0.499 1.174 -2.965")
+    --setXMLString(self.xmlFile, ("vehicle.manureSystemConnectors.connector(%d)#rotation"):format(1), "14.5 180 0")
+    --
+    --setXMLString(self.xmlFile, ("vehicle.manureSystemConnectors.connector(%d)#type"):format(2), "coupling")
+    --setXMLBool(self.xmlFile, ("vehicle.manureSystemConnectors.connector(%d)#isParkPlace"):format(2), true)
+    --setXMLBool(self.xmlFile, ("vehicle.manureSystemConnectors.connector(%d)#createNode"):format(2), true)
+    --setXMLString(self.xmlFile, ("vehicle.manureSystemConnectors.connector(%d)#position"):format(2), "-0.95 1.73 -2.65")
+end
