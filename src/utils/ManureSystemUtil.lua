dbaf62fbd1fc24d2c5c3e48883963cf389d0b2d9
Stijn Wopereis <stijnwop@gmail.com>
2020-02-03 18:44:40 +0100

    Remove unused utility function.

--- src/utils/ManureSystemUtil.lua
+++ src/utils/ManureSystemUtil.lua
@@ -11,17 +11,6 @@ ManureSystemUtil = {}
 
 ManureSystemUtil.NO_RIGID_BODY = "norigidbody"
 
----Gets the spec table for the given spec.
-function ManureSystemUtil.getSpecTable(vehicle, name)
-    local modName = g_manureSystem.modName
-    local spec = vehicle["spec_" .. modName .. "." .. name]
-    if spec ~= nil then
-        return spec
-    end
-
-    return vehicle["spec_" .. name]
-end
-
 ---Gets the first found physics node when the root node isn't a rigid body.
 function ManureSystemUtil.getFirstPhysicsNode(nodeId)
     if getRigidBodyType(nodeId):lower() ~= ManureSystemUtil.NO_RIGID_BODY then
