8b301af400ef37f111919376e4dd2b407fe48c03
Stijn Wopereis <stijnwop@gmail.com>
2020-02-19 23:44:00 +0100

    Add manure system support to the Kuhn Deltis and PF1500.

---
+++ src/vehicles/vanillainserts/ManureSystemKuhnPF1500.lua
@@ -0,0 +1,35 @@
+----------------------------------------------------------------------------------------------------
+-- ManureSystemKuhnPF1500
+----------------------------------------------------------------------------------------------------
+-- Purpose: Insert fertilizer function into the Kuhn PF 1500.
+--
+-- Copyright (c) Wopster, 2019
+----------------------------------------------------------------------------------------------------
+
+---@class ManureSystemKuhnPF1500
+ManureSystemKuhnPF1500 = {}
+
+function ManureSystemKuhnPF1500.prerequisitesPresent(specializations)
+    return true
+end
+
+function ManureSystemKuhnPF1500.registerEventListeners(vehicleType)
+    SpecializationUtil.registerEventListener(vehicleType, "onPreLoad", ManureSystemKuhnPF1500)
+end
+
+function ManureSystemKuhnPF1500:onPreLoad(savegame)
+    setXMLFloat(self.xmlFile, "vehicle.manureSystemPumpMotor#litersPerSecond", 50)
+    setXMLString(self.xmlFile, "vehicle.manureSystemPumpMotor.sounds.pump#template", "SLURRY_02")
+    setXMLFloat(self.xmlFile, "vehicle.manureSystemPumpMotor.sounds.pump.pitch#indoor", 0.85)
+    setXMLFloat(self.xmlFile, "vehicle.manureSystemPumpMotor.sounds.pump.pitch#outdoor", 0.75)
+
+    setXMLString(self.xmlFile, ("vehicle.manureSystemConnectors.connector(%d)#type"):format(0), "couplingFertilizer")
+    setXMLBool(self.xmlFile, ("vehicle.manureSystemConnectors.connector(%d)#createNode"):format(0), true)
+    setXMLString(self.xmlFile, ("vehicle.manureSystemConnectors.connector(%d)#position"):format(0), "0.816 0.67 -0.553")
+    setXMLString(self.xmlFile, ("vehicle.manureSystemConnectors.connector(%d)#rotation"):format(0), "0 90 0")
+
+    setXMLString(self.xmlFile, ("vehicle.manureSystemConnectors.connector(%d)#type"):format(1), "couplingFertilizer")
+    setXMLBool(self.xmlFile, ("vehicle.manureSystemConnectors.connector(%d)#createNode"):format(1), true)
+    setXMLString(self.xmlFile, ("vehicle.manureSystemConnectors.connector(%d)#position"):format(1), "-0.707 0.452 -0.522")
+    setXMLString(self.xmlFile, ("vehicle.manureSystemConnectors.connector(%d)#rotation"):format(1), "90 90 0")
+end
