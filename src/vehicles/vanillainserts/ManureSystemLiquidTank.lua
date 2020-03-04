8ec56856cf43f67ac282c50eaa79bcbec35e804b
Stijn Wopereis <stijnwop@gmail.com>
2020-03-04 21:59:34 +0100

    Remove redundant entries on the vanilla vehicles that use shared sets.

--- src/vehicles/vanillainserts/ManureSystemLiquidTank.lua
+++ src/vehicles/vanillainserts/ManureSystemLiquidTank.lua
@@ -19,10 +19,6 @@ end
 
 function ManureSystemLiquidTank:onPreLoad(savegame)
     setXMLString(self.xmlFile, ("vehicle.manureSystemConnectors.connector(%d)#type"):format(0), "COUPLINGFERTILIZER")
-    setXMLBool(self.xmlFile, ("vehicle.manureSystemConnectors.connector(%d)#createNode"):format(0), true)
-    setXMLString(self.xmlFile, ("vehicle.manureSystemConnectors.connector(%d)#position"):format(0), "0.72 0.33 0")
-    setXMLString(self.xmlFile, ("vehicle.manureSystemConnectors.connector(%d)#rotation"):format(0), "0 90 0")
-
     setXMLBool(self.xmlFile, ("vehicle.manureSystemConnectors.connector(%d).sharedSet#createNode"):format(0), true)
     setXMLInt(self.xmlFile, ("vehicle.manureSystemConnectors.connector(%d).sharedSet#id"):format(0), 3)
     setXMLString(self.xmlFile, ("vehicle.manureSystemConnectors.connector(%d).sharedSet#position"):format(0), "0.638 0.33 0")
