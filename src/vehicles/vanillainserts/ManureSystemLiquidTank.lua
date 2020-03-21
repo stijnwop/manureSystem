13930cffa388605b87ffeae359d862e0cfc8b4d0
Stijn Wopereis <stijnwop@gmail.com>
2020-03-21 16:11:29 +0100

    Don't remove liquid pallet tanks when the fill unit is empty which allows for refilling the tank.

--- src/vehicles/vanillainserts/ManureSystemLiquidTank.lua
+++ src/vehicles/vanillainserts/ManureSystemLiquidTank.lua
@@ -24,4 +24,7 @@ function ManureSystemLiquidTank:onPreLoad(savegame)
     setXMLString(self.xmlFile, ("vehicle.manureSystemConnectors.connector(%d).sharedSet#position"):format(0), "0.638 0.33 0")
     setXMLString(self.xmlFile, ("vehicle.manureSystemConnectors.connector(%d).sharedSet#rotation"):format(0), "0 0 0")
     setXMLString(self.xmlFile, ("vehicle.manureSystemConnectors.connector(%d).sharedSet.connector#type"):format(0), "CONNECTOR_1")
+
+    -- Don't delete the pallet when empty but make it refillable.
+    setXMLBool(self.xmlFile, ("vehicle.fillUnit.fillUnitConfigurations.fillUnitConfiguration(%d).fillUnits#removeVehicleIfEmpty"):format(0), false)
 end
